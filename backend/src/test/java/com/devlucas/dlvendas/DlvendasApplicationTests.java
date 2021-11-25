5e1ea8bc560cd4f42859ef4c70bee18347a8810b
Lusca44 <lucasssrodrigues1@gmail.com>
2021-11-25 09:10:11 -0300

    first commit

---
+++ backend/src/test/java/com/devlucas/dlvendas/DlvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlucas.dlvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
