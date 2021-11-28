4f0754779c8ce805a551f3329d052a087058ec61
Lusca44 <lucasssrodrigues1@gmail.com>
2021-11-27 22:22:09 -0300

    Datatable integration

--- backend/src/main/java/com/devlucas/dlvendas/dto/SaleDTO.java
+++ backend/src/main/java/com/devlucas/dlvendas/dto/SaleDTO.java
@@ -14,19 +14,19 @@ public class SaleDTO implements Serializable{
 	private Double amount;
 	private LocalDate date;
 	
-	private SellerDTO sellerDTO;
+	private SellerDTO seller;
 	
 
 	public SaleDTO() {
 	}
 
-	public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDTO sellerDTO) {
+	public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDTO seller) {
 		this.id = id;
 		this.visited = visited;
 		this.deals = deals;
 		this.amount = amount;
 		this.date = date;
-		this.sellerDTO = sellerDTO;
+		this.seller = seller;
 	}
 
 	public SaleDTO(Sale sale) {
@@ -35,7 +35,7 @@ public class SaleDTO implements Serializable{
 		this.deals = sale.getDeals();
 		this.amount = sale.getAmount();
 		this.date = sale.getDate();
-		this.sellerDTO = new SellerDTO(sale.getSeller());
+		this.seller = new SellerDTO(sale.getSeller());
 	}
 
 	public Long getId() {
@@ -78,11 +78,11 @@ public class SaleDTO implements Serializable{
 		this.date = date;
 	}
 	
-	public SellerDTO getSellerDTO() {
-		return sellerDTO;
+	public SellerDTO getSeller() {
+		return seller;
 	}
 	
-	public void setSellerDTO(SellerDTO sellerDTO) {
-		this.sellerDTO = sellerDTO;
+	public void setSellerDTO(SellerDTO seller) {
+		this.seller = seller;
 	}
 }
